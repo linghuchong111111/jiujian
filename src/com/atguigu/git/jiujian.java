50b5edc45a8f061c767695681208fd6b73c9886d
linghuchong <linghuchong222@126.com>
2017-08-27 10:35:04 +0800

    linghuchong222222

--- src/com/atguigu/git/jiujian.java
+++ src/com/atguigu/git/jiujian.java
@@ -4,6 +4,9 @@ public class jiujian {
 
 	public static void main(String[] args) {
 		System.out.println("HelloWorld");
+		System.out.println("AA");
+		System.out.println("BB");
+		System.out.println("CC");
 
 	}
 
